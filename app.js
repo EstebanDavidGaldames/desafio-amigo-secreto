013188892af0141821e7fb82eed595ff61237859
Esteban David Galdames <esteban.david.galdames@gmail.com>
2025-08-19 02:06:18 -0300

    Validar ingreso de datos y agregar a lista amigos

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 //Lista de amigos
-let amigos = []
-let nombreAmigo = ''
+let amigos = [];
+let nombreAmigo = '';
 
 //Función para agregar amigos
 function agregarAmigo() {
-    nombreAmigo = document.getElementById('amigo').value;
-    console.log(nombreAmigo)
+
+    //Capturamos el input en la variable nombreAmigo
+    //Utilizamos .trim() para eliminar espacios al inicio y al final
+    nombreAmigo = (document.getElementById('amigo').value).trim();
+
+    //Validamos el input para que no sea un campo vacío
+    if (nombreAmigo == '') {
+        alert('Ingrese un nombre');
+    } else {
+        //Agregamos el nombre ingresado en la variable "nombreAmigo" a nuestra lista "amigos"
+        amigos.push(nombreAmigo);
+    }
+
+    console.log(nombreAmigo);
+    console.log(amigos);
+
+
+
     return
 }
